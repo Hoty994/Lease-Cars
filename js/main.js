71e2b1db90fd92e26a348c3930788ad5d37324ef
AlexanderPaduh <sanjapaduh@gmail.com>
2026-01-31 12:49:26 +0100

    Slider dont work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+const swiperTop = new Swiper('.top__swiper', {
+    direction: 'horizontal',
+    loop: true,
+    fadeEffect: {
+        crossFade: true // Слайды плавно накладываются друг на друга
+    },
+    
+    // Включаем свободный режим (листание рукой)
+    freeMode: {
+        enabled: true,
+        // sticky: true ,   // Если false, слайд не будет "прилипать" к краю, можно оставить в любом положении
+        // momentumRatio: 1, // Скорость инерции
+    },
+
+    // Настройки касаний
+    grabCursor: true,
+    touchEventsTarget: 'container',
+    
+    // Параметры скорости
+    speed: 1000, // Скорость переключения при обычном клике
+    
+    // Пагинация (если нужна)
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
