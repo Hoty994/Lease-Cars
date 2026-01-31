537ab4f955ed6f8d821293b772730f7e04e4ddd2
user.email <bohdansav111@gmail.com>
2026-01-31 07:27:55 +0100

    swiper fixxed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
+const slides = document.querySelectorAll(".slides img");
+const slideLine = document.querySelectorAll(".slide-line .line");
+let slideInsex = 0;
+let IntervalId = null;
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if (slideLine.length > 0) {
+            slideLine[0].style.backgroundColor = '#0066FF';
+        }
+
+    if(slides.length > 0){
+        slides[slideInsex].classList.add("displaySlide");
+        IntervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+
+    if(index >= slides.length)
+    {
+        slideInsex = 0;
+    }
+    else if(index < 0) {
+        slideInsex = slides.length - 1;
+    }
+
+
+    slideLine.forEach(line => {
+        line.style.backgroundColor = '#ffffff'; 
+    });
+
+    if (slideLine[slideInsex]) {
+        slideLine[slideInsex].style.backgroundColor = '#0066FF';
+    }
+
+    slides.forEach(slide =>{
+        slide.classList.remove("displaySlide");
+    });
+
+    slides[slideInsex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(IntervalId)
+    slideInsex--;
+    showSlide(slideInsex);
+}
+
+function nextSlide(){
+    slideInsex++;
+    showSlide(slideInsex);
+}
